a33eeccdcac9943ae7a6242be32c94ba04cecf53
nirbhay-design <sharma.59@iitj.ac.in>
2023-02-05 15:46:34 +0530

    adding files

---
+++ DAI/que1.py
@@ -0,0 +1,214 @@
+import yaml
+from yaml.loader import SafeLoader
+import torch
+import torchvision
+import pandas as pd
+import numpy as np
+import os
+from PIL import Image, ImageDraw
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import pickle
+import random
+import time,json
+import copy,sys
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import classification_report,auc,roc_curve,precision_recall_fscore_support
+import warnings
+warnings.filterwarnings("ignore")
+
+def yaml_loader(yaml_file):
+    with open(yaml_file,'r') as f:
+        config_data = yaml.load(f,Loader=SafeLoader)
+        
+    return config_data
+
+def progress(current,total):
+    progress_percent = (current * 50 / total)
+    progress_percent_int = int(progress_percent)
+    print(f" |{chr(9608)* progress_percent_int}{' '*(50-progress_percent_int)}|{current}/{total}",end='\r')
+    if (current == total):
+        print()
+
+class StandardizeTransform(nn.Module):
+    def __init__(self):
+        super(StandardizeTransform, self).__init__()
+        self.transform = None
+        
+    def forward(self, batch_data):
+        """
+        batch_data: [N, 3, W, H]
+        """
+        
+        mean_values = []
+        std_values = []
+        
+        mean_values.append(batch_data[:,0:1,:,:].mean())
+        if batch_data.shape[1] > 1:
+            mean_values.append(batch_data[:,1:2,:,:].mean())
+            mean_values.append(batch_data[:,2:3,:,:].mean())
+        
+        std_values.append(batch_data[:,0:1,:,:].std())
+        if batch_data.shape[1] > 1:
+            std_values.append(batch_data[:,1:2,:,:].std())
+            std_values.append(batch_data[:,2:3,:,:].std())
+        
+        self.transform = torchvision.transforms.Normalize(mean_values, std_values)
+        
+        return self.transform(batch_data)
+
+class Nnet(nn.Module):
+    def __init__(self, nclass=10):
+        super(Nnet, self).__init__()
+        
+        self.layers = nn.Sequential(
+            nn.Conv2d(3,64,3),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.Conv2d(64,128,3),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(128,64,3),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1,1))
+        )
+        
+        self.fc = nn.Linear(64, nclass)
+            
+    def forward(self, x):
+        x = self.layers(x)
+        x = x.flatten(1)
+        x = self.fc(x)
+        return x;
+
+
+def train(model, train_loader, lossfunction, optimizer, transformations, n_epochs, device, return_logs=False): 
+    model = model.to(device)
+    tval = {'trainacc':[],"trainloss":[]}
+    model.train()
+    for epochs in range(n_epochs):
+        cur_loss = 0
+        curacc = 0
+        len_train = len(train_loader)
+        for idx , (data,target) in enumerate(train_loader):
+            if data.shape[0] == 1:
+                continue
+            data = transformations(data)    
+            data = data.to(device)
+            target = target.to(device)
+                
+            scores = model(data)    
+            loss = lossfunction(scores, target)            
+
+            cur_loss += loss.item() / (len_train)
+            scores = F.softmax(scores,dim = 1)
+            _,predicted = torch.max(scores,dim = 1)
+            correct = (predicted == target).sum()
+            samples = scores.shape[0]
+            curacc += correct / (samples * len_train)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        
+            if return_logs:
+                progress(idx+1,len(train_loader))
+      
+        tval['trainacc'].append(float(curacc))
+        tval['trainloss'].append(float(cur_loss))
+        
+        print(f"epochs: [{epochs+1}/{n_epochs}] train_acc: {curacc:.3f} train_loss: {cur_loss:.3f}")
+        
+    return model
+
+def data(config):
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.RandomHorizontalFlip(0.6),
+        torchvision.transforms.RandomRotation(10),
+    ])
+    
+    test_transform = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor()
+    ])
+    dataset_path = config['dataset_path']
+    batch_size = config['batch_size']
+    pin_memory = config['pin_memory']
+    n_workers = config['num_workers']
+    train_data = torchvision.datasets.CIFAR10(dataset_path,train=True,download=True,transform=transform)
+    test_data = torchvision.datasets.CIFAR10(dataset_path,train=False,download=True,transform=test_transform)
+    
+    traindataloader = torch.utils.data.DataLoader(
+        train_data,
+        shuffle=True,
+        batch_size = batch_size,
+        pin_memory=pin_memory,
+        num_workers = n_workers
+    )
+    
+    testdataloader = torch.utils.data.DataLoader(
+        test_data,
+        shuffle=True,
+        batch_size = batch_size,
+        pin_memory=pin_memory,
+        num_workers = n_workers
+    )
+    
+    return traindataloader, testdataloader
+
+def evaluate(model, loader, device, transformations, return_logs=False):
+    correct = 0;samples =0
+
+    with torch.no_grad():
+        loader_len = len(loader)
+        for idx,(x,y) in enumerate(loader):
+            x = transformations(x)
+            x = x.to(device)
+            y = y.to(device)
+
+            scores = model(x)
+            predict_prob = F.softmax(scores,dim=1)
+            _,predictions = predict_prob.max(1)
+            correct += (predictions == y).sum()
+            samples += predictions.size(0)
+        
+            if return_logs:
+                progress(idx+1,loader_len)
+        
+    acc = correct/samples
+    print(f"acc: {acc:.2f}")
+
+    
+if __name__ == "__main__":
+    
+    config = yaml_loader(sys.argv[1])
+    
+    random.seed(config["SEED"])
+    np.random.seed(config["SEED"])
+    torch.manual_seed(config["SEED"])
+    torch.cuda.manual_seed(config["SEED"])
+    torch.backends.cudnn.benchmarks = True
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
+
+        
+    train_data, test_data = data(config)
+     
+    device = torch.device(f'cuda:{config["gpu"]}' if torch.cuda.is_available() else 'cpu')
+    
+    model = Nnet(nclass=config['nclass'])
+    optimizer = optim.SGD(model.parameters(),lr=config['lr'], momentum=config['momentum'])
+    loss = nn.CrossEntropyLoss()
+    
+    transformations = StandardizeTransform()
+    
+    model = train(model, train_data, loss, optimizer, transformations, config['epochs'], device, config['return_logs'])
+    torch.save(model.state_dict(), config['model_saved_path'])
+    
+    evaluate(model, test_data, device, transformations, config['return_logs'])
